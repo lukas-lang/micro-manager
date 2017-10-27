334415c6aa3be5c7b0b0d3aebbe82183c86714d2
langl <langl@ethz.ch>
2017-10-27 14:29:46 +0200

    Support for multiple stages & Fixed bitdepth support

--- DeviceAdapters/FakeCamera/FakeCamera.cpp
+++ DeviceAdapters/FakeCamera/FakeCamera.cpp
@@ -5,15 +5,17 @@
 #include "Util.h"
 
 const char* cameraName = "FakeCamera";
+const char* g_None = "None";
 
 FakeCamera::FakeCamera() :
 	initialized_(false),
 	path_(""),
 	roiX_(0),
 	roiY_(0),
+	capturing_(false),
 	emptyImg(1, 1, CV_8UC1),
 	initSize_(false),
-	curImg_(emptyImg),
+	byteCount_(1),
 	curPath_(""),
 	exposure_(10)
 {
@@ -35,7 +37,18 @@ FakeCamera::FakeCamera() :
 	// binning
 	CreateProperty(MM::g_Keyword_Binning, "1", MM::Integer, false);
 
+	CreateStringProperty(MM::g_Keyword_PixelType, "8bit", false, new CPropertyAction(this, &FakeCamera::OnPixelType));
+
+	std::vector<std::string> pixelTypeValues;
+	pixelTypeValues.push_back("8bit");
+	pixelTypeValues.push_back("16bit");
+
+	SetAllowedValues(MM::g_Keyword_PixelType, pixelTypeValues);
+	
+	SetErrorText(ERR_INVALID_DEVICE_NAME, "Specified stage name is invalid");
 	SetErrorText(OUT_OF_RANGE, "Parameters out of range");
+
+	InitializeDefaultErrorMessages();
 }
 
 FakeCamera::~FakeCamera()
@@ -70,12 +83,14 @@ long FakeCamera::GetImageBufferSize() const
 {
 	initSize();
 
-	return roiWidth_ * roiHeight_;
+	return roiWidth_ * roiHeight_ * byteCount_;
 }
 
 unsigned FakeCamera::GetBitDepth() const
 {
-	return 8 * curImg_.elemSize();
+	initSize();
+
+	return 8 * byteCount_;
 }
 
 int FakeCamera::GetBinning() const
@@ -100,6 +115,8 @@ double FakeCamera::GetExposure() const
 
 int FakeCamera::SetROI(unsigned x, unsigned y, unsigned xSize, unsigned ySize)
 {
+	initSize();
+
 	if (x + xSize > width_ || y + ySize > height_)
 	{
 		return OUT_OF_RANGE;
@@ -144,13 +161,17 @@ int FakeCamera::IsExposureSequenceable(bool & isSequenceable) const
 const unsigned char * FakeCamera::GetImageBuffer()
 {
 	if (!initSize_)
+	{
+		assert(width_ * height_ * byteCount_ == 1);
 		return emptyImg.data;
+	}
 
 	roi_ = curImg_(cv::Range(roiY_, roiY_ + roiHeight_), cv::Range(roiX_, roiX_ + roiWidth_));
 
 	if (!roi_.isContinuous())
 		roi_ = roi_.clone();
 
+	assert(roiWidth_ * roiHeight_ * byteCount_ == roi_.total() * roi_.elemSize());
 	return roi_.data;
 }
 
@@ -170,7 +191,7 @@ unsigned FakeCamera::GetImageHeight() const
 
 unsigned FakeCamera::GetImageBytesPerPixel() const
 {
-	return curImg_.elemSize();
+	return byteCount_;
 }
 
 int FakeCamera::SnapImage()
@@ -192,6 +213,24 @@ ERRH_START
 ERRH_END
 }
 
+int FakeCamera::StartSequenceAcquisition(long numImages, double interval_ms, bool stopOnOverflow)
+{
+	capturing_ = true;
+	return CCameraBase::StartSequenceAcquisition(numImages, interval_ms, stopOnOverflow);
+}
+
+int FakeCamera::StopSequenceAcquisition()
+{
+	capturing_ = false;
+	return CCameraBase::StopSequenceAcquisition();
+}
+
+void FakeCamera::OnThreadExiting() throw()
+{
+	capturing_ = false;
+	CCameraBase::OnThreadExiting();
+}
+
 int FakeCamera::OnPath(MM::PropertyBase * pProp, MM::ActionType eAct)
 {
 	if (eAct == MM::BeforeGet)
@@ -200,6 +239,7 @@ int FakeCamera::OnPath(MM::PropertyBase * pProp, MM::ActionType eAct)
 	}
 	else if (eAct == MM::AfterSet)
 	{
+		std::string oldPath = path_;
 		pProp->Get(path_);
 		initSize_ = false;
 		curPath_ = "";
@@ -208,7 +248,16 @@ int FakeCamera::OnPath(MM::PropertyBase * pProp, MM::ActionType eAct)
 		if (initialized_)
 		{
 			ERRH_START
-				getImg();
+				try
+				{
+					getImg();
+				}
+				catch (error_code ex)
+				{
+					pProp->Set(oldPath.c_str());
+					path_ = oldPath;
+					throw ex;
+				}
 			ERRH_END
 		}
 	}
@@ -216,21 +265,134 @@ int FakeCamera::OnPath(MM::PropertyBase * pProp, MM::ActionType eAct)
 	return DEVICE_OK;
 }
 
+double scaleFac(int bef, int aft)
+{
+	return (double)(1 << (8 * aft)) / (1 << (8 * bef));
+}
+
+int FakeCamera::OnPixelType(MM::PropertyBase * pProp, MM::ActionType eAct)
+{
+	if (eAct == MM::BeforeGet)
+	{
+		pProp->Set(byteCount_ == 1 ? "8bit" : "16bit");
+	}
+	else if (eAct == MM::AfterSet)
+	{
+		if (capturing_)
+			return DEVICE_CAMERA_BUSY_ACQUIRING;
+
+		std::string val;
+		pProp->Get(val);
+
+		byteCount_ = val == "16bit" ? 2 : 1;
+
+		emptyImg.convertTo(emptyImg, byteCount_ == 1 ? CV_8U : CV_16U, scaleFac(emptyImg.elemSize(), byteCount_));
+		if (curImg_.data != NULL)
+			curImg_.convertTo(curImg_, byteCount_ == 1 ? CV_8U : CV_16U, scaleFac(curImg_.elemSize(), byteCount_));
+	}
+
+	return DEVICE_OK;
+}
+
+std::string FakeCamera::buildPath() const throw (error_code)
+{
+	std::ostringstream path;
+
+	path << std::fixed;
+
+	int mode = 0;
+	unsigned long int prec = 0;
+
+	for (const char* it = path_.data(); it != path_.data() + path_.size(); ++it)
+	{
+		switch (mode)
+		{
+		case 0:
+			if (*it == '?')
+				mode = 1;
+			else
+				path << *it;
+			break;
+		case 1:
+			if (*it == '{')
+			{
+				mode = 2;
+				break;
+			}
+		case 3:
+			if (*it == '?')
+			{
+				double pos;
+				int ret = GetCoreCallback()->GetFocusPosition(pos);
+				if (ret != 0)
+					pos = 0;
+
+				if (prec == 0)
+					path << (int)pos;
+				else
+					path << std::setprecision(prec) << pos;
+
+				prec = 0;
+				mode = 0;
+				break;
+			}
+			else if (*it == '[')
+				mode = 4;
+			else
+				throw error_code(CONTROLLER_ERROR, "Invalid path specification. No stage name specified. (format: ?? for focus stage, ?[name] for any stage, and ?{prec}[name]/?{prec}? for precision other than 0)");
+			break;
+		case 2:
+			char* end;
+			prec = strtoul(it, &end, 10);
+			it = end;
+
+			if (prec < 0)
+				prec = 0;
+
+			if (*it == '}')
+				mode = 3;
+			else
+				throw error_code(CONTROLLER_ERROR, "Invalid precision specification. (format: ?? for focus stage, ?[name] for any stage, and ?{prec}[name]/?{prec}? for precision other than 0)");
+			break;
+		case 4:
+			std::ostringstream name;
+			for (; *it != ']' && it != path_.data() + path_.size() - 1; name << *(it++));
+
+			if (*it == ']')
+			{
+				MM::Stage* stage = (MM::Stage*)GetCoreCallback()->GetDevice(this, name.str().c_str());
+				if (!stage)
+					throw error_code(CONTROLLER_ERROR, "Invalid stage name '"+ name.str() + "'. (format: ?? for focus stage, ?[name] for any stage, and ?{prec}[name]/?{prec}? for precision other than 0)");
+
+				double pos;
+				int ret = stage->GetPositionUm(pos);
+				if (ret != 0)
+					pos = 0;
+
+				if (prec == 0)
+					path << (int)pos;
+				else
+					path << std::setprecision(prec) << pos;
+
+				prec = 0;
+				mode = 0;
+			}
+			else
+				throw error_code(CONTROLLER_ERROR, "Invalid name specification. (format: ?? for focus stage, ?[name] for any stage, and ?{prec}[name]/?{prec}? for precision other than 0)");
+		}
+	}
+
+	return path.str();
+}
+
 void FakeCamera::getImg() const throw (error_code)
 {
-	double dPos;
-
-	int ret = GetCoreCallback()->GetFocusPosition(dPos);
-
-	if (ret != 0)
-		throw error_code(CONTROLLER_ERROR, "Focus device not found");
-
-	std::string path = boost::replace_all_copy(path_, "#", to_string((int)dPos));
+	std::string path = buildPath();
 
 	if (path == curPath_)
 		return;
 
-	cv::Mat img = cv::imread(path, cv::IMREAD_GRAYSCALE | cv::IMREAD_ANYDEPTH);
+	cv::Mat img = path == lastFailedPath_ ? lastFailedImg_ : cv::imread(path, cv::IMREAD_GRAYSCALE | cv::IMREAD_ANYDEPTH);
 
 	if (img.data == NULL)
 		if (curImg_.data != NULL)
@@ -240,29 +402,51 @@ void FakeCamera::getImg() const throw (error_code)
 			return;
 		}
 		else
-			throw error_code(CONTROLLER_ERROR, "Could not find image '" + path + "'. Please specify a valid path mask (# is used as placeholder for stage position)");
+			throw error_code(CONTROLLER_ERROR, "Could not find image '" + path + "'. Please specify a valid path mask (format: ?? for focus stage, ?[name] for any stage, and ?{prec}[name]/?{prec}? for precision other than 0)");
+
+	if (img.elemSize() != byteCount_)
+		img.convertTo(img, byteCount_ == 1 ? CV_8U : CV_16U, scaleFac(img.elemSize(), byteCount_));
+
+	if (img.cols != width_ || img.rows != height_)
+		if (capturing_)
+		{
+			lastFailedPath_ = path;
+			lastFailedImg_ = img;
+			throw error_code(DEVICE_CAMERA_BUSY_ACQUIRING);
+		}
+		else
+		{
+			curPath_ = path;
+			curImg_ = img;
+
+			initSize_ = false;
+			initSize(false);
+		}
 
 	curPath_ = path;
 	curImg_ = img;
 }
 
-void FakeCamera::initSize() const
+void FakeCamera::initSize(bool loadImg) const
 {
 	if (initSize_)
 		return;
 
+	initSize_ = true;
+
 	try
 	{
-		getImg();
+		if (loadImg)
+			getImg();
 
 		roiWidth_ = width_ = curImg_.cols;
 		roiHeight_ = height_ = curImg_.rows;
-
-		initSize_ = true;
 	}
 	catch (error_code)
 	{
 		roiWidth_ = width_ = 1;
 		roiHeight_ = height_ = 1;
+
+		initSize_ = false;
 	}
 }
--- DeviceAdapters/FakeCamera/FakeCamera.h
+++ DeviceAdapters/FakeCamera/FakeCamera.h
@@ -6,13 +6,14 @@
 
 #include "opencv/highgui.h"
 
+#define ERR_INVALID_DEVICE_NAME 10000
+#define OUT_OF_RANGE 10001
 #define CONTROLLER_ERROR 10002
 
 #include "error_code.h"
 
 extern const char* cameraName;
-
-#define OUT_OF_RANGE 10001
+extern const char* g_None;
 
 class FakeCamera : public CCameraBase<FakeCamera>
 {
@@ -39,31 +40,41 @@ public:
 	unsigned GetImageHeight() const;
 	unsigned GetImageBytesPerPixel() const;
 	int SnapImage();
+	int StartSequenceAcquisition(long numImages, double interval_ms, bool stopOnOverflow);
+	int StopSequenceAcquisition();
+	void OnThreadExiting() throw();
 
 	int OnPath(MM::PropertyBase* pProp, MM::ActionType eAct);
+	int OnPixelType(MM::PropertyBase* pProp, MM::ActionType eAct);
 
+	std::string buildPath() const throw(error_code);
 	void getImg() const throw (error_code);
 
-	void initSize() const;
+	void initSize(bool loadImg = true) const;
 
 private:
 	bool initialized_;
 
 	std::string path_;
+
+	bool capturing_;
+	mutable bool initSize_;
 	mutable unsigned width_;
 	mutable unsigned height_;
+	mutable unsigned byteCount_;
 
 	mutable unsigned roiX_;
 	mutable unsigned roiY_;
 	mutable unsigned roiWidth_;
 	mutable unsigned roiHeight_;
 
-	const cv::Mat emptyImg;
-	mutable bool initSize_;
+	cv::Mat emptyImg;
 
 	mutable cv::Mat curImg_;
+	mutable cv::Mat lastFailedImg_;
 	cv::Mat roi_;
 	mutable std::string curPath_;
+	mutable std::string lastFailedPath_;
 
 	double exposure_;
 };
