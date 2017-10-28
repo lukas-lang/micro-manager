83c28f84e1d4778184d5c2329847c85cf4ba501b
langl <langl@ethz.ch>
2017-10-28 22:05:32 +0200

    Added license header

--- DeviceAdapters/FakeCamera/error_code.h
+++ DeviceAdapters/FakeCamera/error_code.h
@@ -1,3 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          error_code.h
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   A camera implementation that is backed by the file system
+//                Can access stage positions to choose image to display
+//
+// AUTHOR:        Lukas Lang
+//
+// COPYRIGHT:     2017 Lukas Lang
+// LICENSE:       Licensed under the Apache License, Version 2.0 (the "License");
+//                you may not use this file except in compliance with the License.
+//                You may obtain a copy of the License at
+//                
+//                http://www.apache.org/licenses/LICENSE-2.0
+//                
+//                Unless required by applicable law or agreed to in writing, software
+//                distributed under the License is distributed on an "AS IS" BASIS,
+//                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//                See the License for the specific language governing permissions and
+//                limitations under the License.
+
 #pragma once
 
 #include <exception>
