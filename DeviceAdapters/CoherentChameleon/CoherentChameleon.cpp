c1a1e21fb2e9fe8493227cc54377ff1f0f5175ab
langl <langl@ethz.ch>
2017-11-01 16:23:28 +0100

    Added license headers

--- DeviceAdapters/CoherentChameleon/CoherentChameleon.cpp
+++ DeviceAdapters/CoherentChameleon/CoherentChameleon.cpp
@@ -1,3 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          CoherentChameleon.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   A device adapter for the Coherent Chameleon Ultra laser
+//
+// AUTHOR:        Lukas Lang
+//
+// COPYRIGHT:     2017 Lukas Lang
+// LICENSE:       Licensed under the Apache License, Version 2.0 (the "License");
+//                you may not use this file except in compliance with the License.
+//                You may obtain a copy of the License at
+//                
+//                http://www.apache.org/licenses/LICENSE-2.0
+//                
+//                Unless required by applicable law or agreed to in writing, software
+//                distributed under the License is distributed on an "AS IS" BASIS,
+//                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//                See the License for the specific language governing permissions and
+//                limitations under the License.
+
 #include <algorithm>
 #include <math.h>
 #include <sstream>
@@ -13,7 +35,7 @@
 #include "fault_codes.h"
 
 // Controller
-const char* g_device_name = "Coherent chameleon laser";
+const char* g_device_name = "Coherent chameleon Ultra laser";
 
 ///////////////////////////////////////////////////////////////////////////////
 // Controller implementation
@@ -25,10 +47,10 @@ CoherentChameleon::CoherentChameleon() :
 	InitializeDefaultErrorMessages();
 
 	// Name
-	CreateProperty(MM::g_Keyword_Name, "Coherent chameleon laser", MM::String, true);
+	CreateProperty(MM::g_Keyword_Name, g_device_name, MM::String, true);
 
 	// Description
-	CreateProperty(MM::g_Keyword_Description, "Device adapter for the Coherent Chameleon laser", MM::String, true);
+	CreateProperty(MM::g_Keyword_Description, "Device adapter for the Coherent Chameleon Ultra laser", MM::String, true);
 
 	// Port
 	CPropertyAction* pAct = new CPropertyAction(this, &CoherentChameleon::OnPort);
--- DeviceAdapters/CoherentChameleon/fault_codes.h
+++ DeviceAdapters/CoherentChameleon/fault_codes.h
@@ -1,3 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          fault_code.h
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   A device adapter for the Coherent Chameleon Ultra laser
+//
+// AUTHOR:        Lukas Lang
+//
+// COPYRIGHT:     2017 Lukas Lang
+// LICENSE:       Licensed under the Apache License, Version 2.0 (the "License");
+//                you may not use this file except in compliance with the License.
+//                You may obtain a copy of the License at
+//                
+//                http://www.apache.org/licenses/LICENSE-2.0
+//                
+//                Unless required by applicable law or agreed to in writing, software
+//                distributed under the License is distributed on an "AS IS" BASIS,
+//                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//                See the License for the specific language governing permissions and
+//                limitations under the License.
+
 #pragma once
 
 #include <string>
